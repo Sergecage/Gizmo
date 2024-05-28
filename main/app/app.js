fbbc264f7f9455c212e0f5c8a7725bb19f73dad8
Serge Cage <sergeycage@rocketmail.com>
2024-05-28 23:05:04 +0300

    feat:update score function, add reset item function

--- main/app/app.js
+++ main/app/app.js
@@ -2,17 +2,19 @@ const content = document.querySelector(".main");
 //const graphics = document.selectElementByTagName('svg')[0];
 const leftSide = document.querySelector(".left-side");
 const rightSide = document.querySelector(".right-side");
-const gizmo = document.querySelector(".gizmo-img");
+const gizmoImg = document.querySelector(".gizmo-img");
+const gizmo = document.querySelector(".gizmo");
 const playArea = document.querySelector(".area");
 
+let score = 0;
 
 //Gizmo moves
 rightSide.addEventListener("mouseover", event => {
-    gizmo.src = `./img/pictures/Gizmo-right.png`;
+    gizmoImg.src = `./img/pictures/Gizmo-right.png`;
 });
 
 leftSide.addEventListener("mouseover", event => {
-    gizmo.src = `./img/pictures/Gizmo-left.png`;
+    gizmoImg.src = `./img/pictures/Gizmo-left.png`;
 })
 
 //items to catch
@@ -42,7 +44,7 @@ const items = [{
 },
 {
     name: "rock",
-    meaning: "food",
+    meaning: "thing",
     bonus: "0",
     img: "img/pictures/rock.png"
 }];
@@ -52,40 +54,100 @@ const itemContainer = document.createElement("div");
     itemContainer.className = "play-item";
     const itemImg = document.createElement("img");
     itemImg.className = "item";
+    
+    
+
+let itemContainerX = Math.random() * (playArea.offsetWidth - itemContainer.offsetWidth);
+let gizmoX = playArea.offsetWidth / 2 - gizmo.offsetWidth /2;
+let itemContainerY = 0;
+//create item for the game
+const generateItem = () => {
     const randomImg =items[Math.floor(Math.random() *items.length)];
     itemImg.src = `${randomImg.img}`;
     itemImg.alt = `${randomImg.name}`;
+    const randomMeaning = `${randomImg.meaning}`;
+    console.log(randomMeaning);
     itemContainer.append(itemImg);
+    playArea.append(itemContainer);
+    if (randomMeaning == "food"){
+    updateScore();
+    } else {
+        decreaseScore();
+    }
+}
 
-//create item for the game
-const generateItem = () => {
-    leftSide.append(itemContainer);
+//update score
+const updateScore = () => {
+        scoreItem.innerText = parseInt(score++);
+}
+
+const decreaseScore = () => {
+    scoreItem.innerText = parseInt(score--);
 }
 
 
 //drop item function
 const dropItem =  () => {
+    itemContainerY += 1;
+    itemContainer.style.top = `${itemContainerY}px`;
+    itemContainer.style.left = `${itemContainerX}px`
+
+    if (itemContainerY > playArea.offsetHeight - gizmo.offsetHeight / 1.6 &&
+        itemContainerX + itemContainer.offsetWidth > gizmoX &&
+        itemContainerX < gizmoX + gizmo.offsetWidth) { 
+            resetItem();
+        } else if ( itemContainerY > playArea.offsetHeight) {
+            resetItem();
+        } else {
+            requestAnimationFrame(dropItem);
+        }
+}
+
+//reset item
+const resetItem = () => {
+    itemContainerY = 0;
+    itemContainerX = Math.random() * (playArea.offsetWidth - itemContainer.offsetWidth);
+    dropItem();
     generateItem();
-    //gizmo.classList.add("left");
-    itemContainer.classList.add("active");
 }
 //create start button
 const startBtn = document.createElement("div");
     startBtn.className = "start-btn";
     startBtn.innerText = "start";
-    playArea.append(startBtn);
+    content.append(startBtn);
+
+//score
+const scoreItem = document.createElement("div");
+scoreItem.className = "score";
+scoreItem.innerText = "0";
+content.append(scoreItem);
 //start game 
-const startGame = () => {
-    dropItem();
-}
+
 //move Gizmo
 const moveGizmo = (button) => {
+    const move = 50;
+    if (button.key === "ArrowLeft" && gizmoX > 0) {
+        gizmoX -= move;
+        gizmoImg.src = `./img/pictures/Gizmo-left.png`;
+    }
+     if (button.key === "ArrowRight" && gizmoX < playArea.offsetWidth - gizmo.offsetWidth){
+        gizmoX += move;
+        gizmoImg.src = `./img/pictures/Gizmo-right.png`;
+    }
+    gizmo.style.left = `${gizmoX}px`;
+    console.log("move");
+}
+
+
+startBtn.addEventListener("click", dropItem);
+document.addEventListener("keydown", moveGizmo);
+
+/*const discoGizmo (button) => {
+    itemContainer.classList.add("active");
     if (button.key.startsWith("ArrowLeft")) {
-        gizmo.classList.add("left");
+        gizmo.classList.toggle("left");
     }
     else if (button.key.startsWith("ArrowRight")){
-        gizmo.classList.add("right");
+        gizmo.classList.toggle("right");
     }
-}
-startBtn.addEventListener("click", startGame);
-document.addEventListener("keydown", moveGizmo);
+}*/
