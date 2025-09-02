f91b498d77eb9a80071ea03661ddc9e6f09dcf12
Serge Cage <sergeycage@rocketmail.com>
2025-09-02 22:59:55 +0300

    feat: style canvas add player image

--- Gizmo2/game/game.js
+++ Gizmo2/game/game.js
@@ -1,6 +1,21 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const CANVAS_WIDTH = canvas.width = 1200;
+const CANVAS_HEIGHT = canvas.width = 1200;
+
+const playerImg = new Image();
+playerImg.src = '../assets/img/Gizmo.png';
+
+const animate = () => {
+    ctx.clearRect(0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.fillRect(50, 50, 100,100);
+    ctx.drawImage(playerImg, 0, 0);
+    requestAnimationFrame(animate);
+}
+
+animate();
+
 const updateState = () => {
 
 }
