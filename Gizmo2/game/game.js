3a447914a77f491d4df88d32631ef819865b8bcc
Serge Cage <sergeycage@rocketmail.com>
2025-09-10 00:12:42 +0300

    feat: add variables for different animations

--- Gizmo2/game/game.js
+++ Gizmo2/game/game.js
@@ -6,10 +6,11 @@ const CANVAS_HEIGHT = canvas.width = 1200;
 
 const playerImg = new Image();
 playerImg.src = '../assets/img/Gizmo.png';
+let frameX = 0;
+let frameY = 0;
 
 const animate = () => {
     ctx.clearRect(0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
-    //ctx.fillRect(100, 50, 100,100);
     ctx.drawImage(playerImg, 0, 180, 300, 300,);
     requestAnimationFrame(animate);
 }
