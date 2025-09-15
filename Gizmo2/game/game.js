f1cbaa7157814309b52b85946fc191544890e951
Serge Cage <sergeycage@rocketmail.com>
2025-09-15 23:28:16 +0300

    feat: make animation of player

--- Gizmo2/game/game.js
+++ Gizmo2/game/game.js
@@ -5,15 +5,23 @@ const CANVAS_WIDTH = canvas.width = 1200;
 const CANVAS_HEIGHT = canvas.width = 1200;
 
 const playerImg = new Image();
-playerImg.src = '../assets/img/Gizmo_jump.png';
-const spriteWidth = 575;
+playerImg.src = '../assets/img/Gizmo_walk_cycle.png';
+const spriteWidth = 465;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
+let gameFrame = 0;
+const staggerFrame = 5;
 
 const animate = () => {
     ctx.clearRect(0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    if (gameFrame % staggerFrame == 0) {
+        if (frameX < 3) frameX++;
+        else frameX = 0;
+    }
+
+    gameFrame++;
     requestAnimationFrame(animate);
 }
 
