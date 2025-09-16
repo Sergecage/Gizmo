19b1e75fa09f72e244db12a63a0e751e2aa2d88e
Serge Cage <sergeycage@rocketmail.com>
2025-09-16 21:49:58 +0300

    refactor: rewrite animation

--- Gizmo2/game/game.js
+++ Gizmo2/game/game.js
@@ -6,7 +6,7 @@ const CANVAS_HEIGHT = canvas.width = 1200;
 
 const playerImg = new Image();
 playerImg.src = '../assets/img/Gizmo_walk_cycle.png';
-const spriteWidth = 465;
+const spriteWidth = 445;
 const spriteHeight = 523;
 let frameX = 0;
 let frameY = 0;
@@ -15,11 +15,9 @@ const staggerFrame = 5;
 
 const animate = () => {
     ctx.clearRect(0, 0,CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImg, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    if (gameFrame % staggerFrame == 0) {
-        if (frameX < 3) frameX++;
-        else frameX = 0;
-    }
+    let position = Math.floor(gameFrame / staggerFrame) % 2;
+    frameX = spriteWidth * position;
+    ctx.drawImage(playerImg, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
 
     gameFrame++;
     requestAnimationFrame(animate);
