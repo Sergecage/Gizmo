138151d270efcee97bb36b19418ccc83fae853be
Serge Cage <sergeycage@rocketmail.com>
2025-09-21 22:43:13 +0300

    refactor: delete uneccessary code

--- Gizmo2/game/game.js
+++ Gizmo2/game/game.js
@@ -8,11 +8,11 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 const CANVAS_WIDTH = canvas.width = 1200;
-const CANVAS_HEIGHT = canvas.width = 1200;
+const CANVAS_HEIGHT = canvas.width =1200;
 
 const playerImg = new Image();
 playerImg.src = '../assets/img/Gizmo_walk_cycle.png';
-const spriteWidth = 445;
+const spriteWidth = 435;
 const spriteHeight = 523;
 let gameFrame = 0;
 const staggerFrame = 15;
