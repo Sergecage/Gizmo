058a001b7eb54dbb17322c130d8fa4d7dd89c079
Serge Cage <sergeycage@rocketmail.com>
2025-09-27 22:32:03 +0300

    refactor: refactor of background animation

--- Gizmo2/game/game.js
+++ Gizmo2/game/game.js
@@ -77,20 +77,15 @@ class Layer {
         this.y = 0;
         this.width = 2400;
         this.height = 450;
-        this.x2 = this.width;
         this.speedModifier = speedModifier;
         this.speed = gameSpeed * this.speedModifier;
     }
     update(){
         this.speed = gameSpeed * this.speedModifier;
         if (this.x <= -this.width) {
-            this.x = this.width + this.x2 - this.speed;
-        }
-        if (this.x2 <= -this.width) {
-            this.x2 = this.width + this.x - this.speed;
+            this.x = 0;
         }
         this.x =  Math.floor(this.x - this.speed);
-        this.x2 = Math.floor(this.x2 - this.speed);
     }
     draw(){
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
