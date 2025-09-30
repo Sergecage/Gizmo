5030dbc2f464bfb5d28196702ceb4607e8656699
Serge Cage <sergeycage@rocketmail.com>
2025-09-30 20:54:42 +0300

    feat: create file for enemy sprite

---
+++ Gizmo2/game/enemy.js
@@ -0,0 +1,6 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const CANVAS_WIDTH = canvas.width = 1200;
+const CANVAS_HEIGHT = canvas.width =1200;
