00d82ac48b0566fe15a5a0251943450fce15294d
Serge Cage <sergeycage@rocketmail.com>
2025-09-30 20:57:53 +0300

    feat: create object for enemy as template

--- Gizmo2/game/enemy.js
+++ Gizmo2/game/enemy.js
@@ -4,3 +4,16 @@ const ctx = canvas.getContext("2d");
 
 const CANVAS_WIDTH = canvas.width = 1200;
 const CANVAS_HEIGHT = canvas.width =1200;
+
+enemy1 = {
+    x: 0,
+    y: 0,
+    width: 200,
+    height: 200,
+}
+
+const animateEnemy1 = () => {
+    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+}
+
+animateEnemy1();
