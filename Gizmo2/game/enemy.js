8cf9b9b31beab5be8cc04214f7ff7a86d1c6eb80
Serge Cage <sergeycage@rocketmail.com>
2025-10-01 15:21:02 +0300

    refactor: refactor enemy property with class enemy

--- Gizmo2/game/enemy.js
+++ Gizmo2/game/enemy.js
@@ -5,12 +5,15 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 1200;
 const CANVAS_HEIGHT = canvas.width =1200;
 
-enemy1 = {
-    x: 0,
-    y: 0,
-    width: 200,
-    height: 200,
+class Enemy {
+    constructor(){
+        this.x = 10;
+        this.y = 50;
+        this.width = 100;
+        this.height = 100;
+    }
 }
+const enemy1 = new Enemy();
 
 const animateEnemy1 = () => {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
