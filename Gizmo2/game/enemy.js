0b6e56726128f8448f92661ddf08a630a6080995
Serge Cage <sergeycage@rocketmail.com>
2025-10-01 15:29:26 +0300

    refactor: update enemy position by random

--- Gizmo2/game/enemy.js
+++ Gizmo2/game/enemy.js
@@ -7,14 +7,17 @@ const CANVAS_HEIGHT = canvas.width =1200;
 
 class Enemy {
     constructor(){
-        this.x = 10;
-        this.y = 50;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
         this.width = 100;
         this.height = 100;
     }
     update(){
         this.x++;
         this.y++;
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
     }
 }
 const enemy1 = new Enemy();
@@ -23,7 +26,7 @@ const enemy2 = new Enemy();
 const animateEnemy1 = () => {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     enemy1.update();
-    ctx.fillRect(enemy1.x, enemy1.y, enemy1.width, enemy1.height);
+    enemy1.draw();
     requestAnimationFrame(animateEnemy1);
 }
 
