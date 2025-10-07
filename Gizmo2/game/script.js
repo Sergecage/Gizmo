63c5f96663ed19133d4cec59085204e1985e769d
Serge Cage <sergeycage@rocketmail.com>
2025-10-07 23:17:18 +0300

    feat: create class for colision

--- Gizmo2/game/script.js
+++ Gizmo2/game/script.js
@@ -1,8 +1,11 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
-
 const CANVAS_WIDTH = canvas.width = 1200;
 const CANVAS_HEIGHT = canvas.width =1200;
+const explosions = [];
 
-ctx.fillStyle = "white";
-ctx.fillRect(50, 50, 100, 150);
+class Explosion{
+    constructor(x, y){
+
+    }
+}
