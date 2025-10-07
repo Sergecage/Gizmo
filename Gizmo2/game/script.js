9c92f3ce685e1e583ade6413c5f182b8e9ae9197
Serge Cage <sergeycage@rocketmail.com>
2025-10-07 23:15:40 +0300

    feat: add test player

--- Gizmo2/game/script.js
+++ Gizmo2/game/script.js
@@ -3,3 +3,6 @@ const ctx = canvas.getContext("2d");
 
 const CANVAS_WIDTH = canvas.width = 1200;
 const CANVAS_HEIGHT = canvas.width =1200;
+
+ctx.fillStyle = "white";
+ctx.fillRect(50, 50, 100, 150);
