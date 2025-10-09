c0cddc301b083ec4faa8576acb6d4a61227a3f57
Serge Cage <sergeycage@rocketmail.com>
2025-10-09 21:19:56 +0300

    feat: ad audio file

--- Gizmo2/game/colision.js
+++ Gizmo2/game/colision.js
@@ -18,6 +18,7 @@ class Explosion{
         this.frame = 0;
         this.timer = 0;
         this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
     }
     update(){
         this.frame++;
