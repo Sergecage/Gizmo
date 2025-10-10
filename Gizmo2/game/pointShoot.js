63ede83120273b9e4b9376b486479104a8659b44
Serge Cage <sergeycage@rocketmail.com>
2025-10-10 23:02:44 +0300

    feat: set up the direction of moving of the enemy

--- Gizmo2/game/pointShoot.js
+++ Gizmo2/game/pointShoot.js
@@ -9,6 +9,8 @@ class Bat{
         this.width = 100;
         this.height = 50;
         this.x = canvas.width;
-        this.y = Math.random() * canvas.height;
+        this.y = Math.random() * canvas.height - this.height;
+        this.directionX = Math.random() * 5 + 3;
+        this.directionY = Math.random() * 5 - 2.5; 
     }
 }
