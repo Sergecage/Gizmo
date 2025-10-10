c0c0cb3cdf7b19cd5ccd91760aa2da87a9931821
Serge Cage <sergeycage@rocketmail.com>
2025-10-10 22:56:55 +0300

    feat: set up class position

--- Gizmo2/game/pointShoot.js
+++ Gizmo2/game/pointShoot.js
@@ -6,6 +6,9 @@ const CANVAS_HEIGHT = canvas.width =1200;
 let bats = [];
 class Bat{
     constructor(x, y){
-        
+        this.width = 100;
+        this.height = 50;
+        this.x = canvas.width;
+        this.y = Math.random() * canvas.height;
     }
 }
