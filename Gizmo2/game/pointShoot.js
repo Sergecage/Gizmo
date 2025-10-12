bf2d10538f5bd62414ced707024d20474939480e
Serge Cage <sergeycage@rocketmail.com>
2025-10-12 22:52:37 +0300

    feat: set max number of frames

--- Gizmo2/game/pointShoot.js
+++ Gizmo2/game/pointShoot.js
@@ -33,7 +33,7 @@ class Bat{
     }
     draw(){
         ctx.strokeRect(this.x, this.y, this.width, this.height);
-        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
