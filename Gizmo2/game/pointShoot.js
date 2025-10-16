811bb79022c4b48024026e5209497391b9aca01f
Serge Cage <sergeycage@rocketmail.com>
2025-10-16 23:19:36 +0300

    feat: increase time by deltatime

--- Gizmo2/game/pointShoot.js
+++ Gizmo2/game/pointShoot.js
@@ -69,6 +69,9 @@ class Explosion {
     }
     update(deltatime){
         this.timeSinceLastFrame += deltatime;
+        if (this.timeSinceLastFrame > this.frameInterval){
+            this.frame++;
+        }
     }
 }
 
