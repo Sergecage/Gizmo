2f9be48979c44d249e5a3c2cf3bda5937cbcbe46
Serge Cage <sergeycage@rocketmail.com>
2025-10-21 23:30:06 +0300

    feat: set up spawn interval

--- Gizmo2/game/enemies.js
+++ Gizmo2/game/enemies.js
@@ -11,7 +11,7 @@ class Game{
         this.width = width;
         this.height - height;
         this.enemies = [];
-        this.enemyInterval = 400;
+        this.enemyInterval = 1000;
         this.enemyTimer = 0;
     }
     update(deltaTime){
