7e66b500c8d7765c4f1f6fac604d4051678c8361
Serge Cage <sergeycage@rocketmail.com>
2025-10-25 23:17:49 +0300

    feat: add width and height to initial enemy class

--- Gizmo2/game/enemies.js
+++ Gizmo2/game/enemies.js
@@ -42,7 +42,7 @@ class Enemy {
         if (this.x < 0 - this.width) this.markedForDeletion = true;
     }
     draw(ctx){
-        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, 0, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
     }
 }
 
