ddab9eb60f1bc659aee474396ab7d1b535bdb0ff
Serge Cage <sergeycage@rocketmail.com>
2025-10-31 23:22:29 +0300

    feat: set up width and height of canvas

--- Gizmo2/game/move.js
+++ Gizmo2/game/move.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function() {
     const canvas = document.getElementById("game");
     const ctx = canvas.getContext("2d");
+    canvas.width = 800;
+    canvas.height = 720;
 });
