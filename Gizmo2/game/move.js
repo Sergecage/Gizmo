a38e4b23a913d0657ddd15b4a6d4a59f9ca7a9de
Serge Cage <sergeycage@rocketmail.com>
2025-11-01 23:54:47 +0300

    feat: ad event listener on all arrow buttons

--- Gizmo2/game/move.js
+++ Gizmo2/game/move.js
@@ -8,7 +8,8 @@ window.addEventListener("load", function() {
         constructor(){
             this.keys = [];
             window.addEventListener("keydown",  e => {
-                if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1){
+                if ((e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === "ArrowLeft" || e.key === "ArrowRight") 
+                    && this.keys.indexOf(e.key) === -1){
                     this.keys.push(e.key);
                 }
             })
