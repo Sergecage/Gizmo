0e44b1fc2a9baca1afd05af131c598da61488f81
Serge Cage <sergeycage@rocketmail.com>
2025-11-01 23:45:15 +0300

    feat: add button elemnt on keydown

--- Gizmo2/game/move.js
+++ Gizmo2/game/move.js
@@ -7,8 +7,10 @@ window.addEventListener("load", function() {
     class InputHandler{
         constructor(){
             this.keys = [];
-            window.addEventListener("keydown",  function(){
-
+            window.addEventListener("keydown",  function(e){
+                if (e.key === "ArrowDown"){
+                    this.keys.push(e.key);
+                }
             })
         }
     }
