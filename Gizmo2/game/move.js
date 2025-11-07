44a14c5afc1dc246617b8156d3219b2cc822a542
Serge Cage <sergeycage@rocketmail.com>
2025-11-07 23:37:06 +0300

    feat: update gameover text

--- Gizmo2/game/move.js
+++ Gizmo2/game/move.js
@@ -203,9 +203,9 @@ window.addEventListener("load", function() {
         if (gameOver) {
             context.textAlign = "center";
             context.fillStyle = "black";
-            context.fillText("Game Over!", canvas.width / 2, 100);
+            context.fillText("Game Over! Press ENTER to restart", canvas.width / 2, 100);
             context.fillStyle = "orange";
-            context.fillText("Game Over!", canvas.width / 2 + 2, 100);
+            context.fillText("Game Over! Press ENTER to restart", canvas.width / 2 + 2, 100);
         }
     };
 
