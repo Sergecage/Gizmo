1e59b1ef0d83fae59e64a8b4252b05d8b9ecd95c
Serge Cage <sergeycage@rocketmail.com>
2025-11-07 16:41:07 +0300

    fix: fix collision

--- Gizmo2/game/move.js
+++ Gizmo2/game/move.js
@@ -141,8 +141,12 @@ window.addEventListener("load", function() {
             this.markedForDeletion = false;
         }
         draw(context){
-            //context.strokeStyle = "white";
-            //context.strokeRect(this.x, this.y, this.width, this.height);
+            context.strokeStyle = "white";
+            context.strokeRect(this.x, this.y, this.width, this.height);
+            context.beginPath();
+            context.arc(this.x + this.width / 2 , this.y + this.height /2, 0, this.width / 2, Math.PI * 2);
+            context.stroke();
+            context.strokeStyle = "orange";
             context.beginPath();
             context.arc(this.x + this.width / 2 , this.y + this.height /2, 0, this.width / 2, Math.PI * 2);
             context.stroke();
