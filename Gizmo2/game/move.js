4d2100d1ede308dae81bf83144cdb06806376b9f
Serge Cage <sergeycage@rocketmail.com>
2025-11-09 23:12:18 +0300

    feat: jump on swipe

--- Gizmo2/game/move.js
+++ Gizmo2/game/move.js
@@ -101,7 +101,7 @@ window.addEventListener("load", function() {
                 this.speed = 5;
             } else if (input.keys.indexOf("ArrowLeft") > - 1){
                 this.speed = -5;
-            } else if (input.keys.indexOf("ArrowUp") > - 1 && this.onGround()){
+            } else if ((input.keys.indexOf("ArrowUp") > - 1 || input.keys.indexOf("swipe up")) && this.onGround()){
                 this.vy = -32;
             }
             else {
