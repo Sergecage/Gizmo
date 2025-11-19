58a3521694519359d9f067e73fa4bfbb368513d8
Serge Cage <sergeycage@rocketmail.com>
2025-11-19 13:50:35 +0300

    feat: make canvas 2d

--- Gizmo2/game/main.js
+++ Gizmo2/game/main.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function(){
     const loading = document.getElementById("loading");
-    loading.style.display = "none;"
+    loading.style.display = "none";
+    const canvas = this.document.getElementById("game-frame");
+    const ctx = canvas.getContext("2d");
 })
