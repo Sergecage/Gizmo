56800ac3f3bfb638371fd00183916e411a79dedf
Serge Cage <sergeycage@rocketmail.com>
2025-11-19 23:10:41 +0300

    feat: set up image width and height

--- Gizmo2/js/player.js
+++ Gizmo2/js/player.js
@@ -5,5 +5,7 @@ class Player {
         this.states = [];
         this.currentState = this.staets[0];
         this.image = document.getElementById("player");
+        this.width = 250;
+        this.height = 180;
     }
 }
