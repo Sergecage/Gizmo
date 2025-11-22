a2bf71954a6410a0976ecce193907bd5f4382fb5
Serge Cage <sergeycage@rocketmail.com>
2025-11-22 23:35:36 +0300

    feat: add keydown event listener

--- Gizmo2/js/input.js
+++ Gizmo2/js/input.js
@@ -1,5 +1,11 @@
 export default class InputHandler {
     constructor(){
         this.lastKey = "";
+        window.addEventListener("keydown", function(e) {
+            switch(e.key){
+                case "ArrowLeft":
+                    this.lastKey = "PRESS left";
+            }
+        })
     }
 }
