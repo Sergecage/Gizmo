d45462e4e773d75caa364266ce56ab55654c4a0c
Serge Cage <sergeycage@rocketmail.com>
2025-11-23 20:51:07 +0300

    feat: style drawStatus

--- Gizmo2/js/utils.js
+++ Gizmo2/js/utils.js
@@ -1,4 +1,4 @@
 export const drawStatus = (context, input) => {
     context.font = "30px Helvetica";
-    context.fillText("Last Input: " + input.lastKey, 10, 20); 
+    context.fillText("Last Input: " + input.lastKey, 20, 50); 
 }
