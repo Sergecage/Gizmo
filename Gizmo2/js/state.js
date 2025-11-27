402097e2afcb8e7238d52746d780d6ce405b5e1a
Serge Cage <sergeycage@rocketmail.com>
2025-11-27 16:15:07 +0300

    feat: change to standing on release

--- Gizmo2/js/state.js
+++ Gizmo2/js/state.js
@@ -49,6 +49,7 @@ class SittingLeft extends State {
     handleInput(input){
         if (input === "PRESS right") this.player.setState(states.SITTING_RIGHT);
         else if (input === "PRESS up") this.player.setState(states.STANDING_LEFT);
+        else if (input === "RELEASASE down") this.player.setState(states.STANDING_LEFT);
     }
 }
 class SittingRight extends State {
@@ -62,5 +63,6 @@ class SittingRight extends State {
     handleInput(input){
         if (input === "PRESS left") this.player.setState(states.SITTING_LEFT);
         else if (input === "PRESS up") this.player.setState(states.STANDING_RIGHT);
+        else if (input === "RELEASASE down") this.player.setState(states.STANDING_RIGHT);
     }
 }
