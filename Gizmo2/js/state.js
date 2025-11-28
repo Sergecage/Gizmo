3433bfbe1780bc67bb4fda66f3fc1e4badb7711a
Serge Cage <sergeycage@rocketmail.com>
2025-11-28 20:47:45 +0300

    feat: add velocity to state

--- Gizmo2/js/state.js
+++ Gizmo2/js/state.js
@@ -115,6 +115,7 @@ class JumpingLeft extends State {
     }
     enter(){
         this.player.frameY = 3;
+        this.vy -= 20;
     }
     handleInput(input){
         if (input === "PRESS left") this.player.setState(states.RUNNING_LEFT);
@@ -127,6 +128,7 @@ class JumpingRight extends State {
     }
     enter(){
         this.player.frameY = 2;
+         this.vy -= 20;
     }
     handleInput(input){
         if (input === "PRESS left") this.player.setState(states.RUNNING_LEFT);
