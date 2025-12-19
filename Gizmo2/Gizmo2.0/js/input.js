18da70330e6c2132b1c9ed620d140383e3018dee
Serge Cage <sergeycage@rocketmail.com>
2025-12-19 23:18:49 +0300

    feat: check if arrowdown is pressed

--- Gizmo2/Gizmo2.0/js/input.js
+++ Gizmo2/Gizmo2.0/js/input.js
@@ -1,6 +1,10 @@
 export class InputHandler {
     constructor(){
         this.keys = [];
-        windows.addEventListener('keydown', )
+        windows.addEventListener('keydown', e => {
+            if (e.key === "ArrowDown" && this.key.indexOf(e.key) === -1){
+                this.key.push(e.key);
+            } 
+        });
     }
 }
