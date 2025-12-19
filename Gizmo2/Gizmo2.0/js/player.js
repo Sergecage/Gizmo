b950cea509d21550eca964e2453e403639f24fe0
Serge Cage <sergeycage@rocketmail.com>
2025-12-19 22:26:43 +0300

    feat: animate sprite

--- Gizmo2/Gizmo2.0/js/player.js
+++ Gizmo2/Gizmo2.0/js/player.js
@@ -8,7 +8,7 @@ export class Player {
         this.image = gizmo;
     }
     update(){
-
+        this.x++;
     }
     draw(context){
         context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height);
