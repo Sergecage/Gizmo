5285576092ca0b1cf8ce5c9146a904babdfaae2a
Serge Cage <sergeycage@rocketmail.com>
2025-12-19 23:19:53 +0300

    fix: fix inputHandler

--- Gizmo2/Gizmo2.0/js/input.js
+++ Gizmo2/Gizmo2.0/js/input.js
@@ -1,7 +1,7 @@
 export class InputHandler {
     constructor(){
         this.keys = [];
-        windows.addEventListener('keydown', e => {
+        window.addEventListener('keydown', e => {
             if (e.key === "ArrowDown" && this.key.indexOf(e.key) === -1){
                 this.key.push(e.key);
             } 
