edb08558cecd8a9560a78fe5d6b609afc66b0b0f
Serge Cage <sergeycage@rocketmail.com>
2025-12-20 22:18:10 +0300

    feat: add event for arrowLeft and Right

--- Gizmo2/Gizmo2.0/js/input.js
+++ Gizmo2/Gizmo2.0/js/input.js
@@ -2,7 +2,8 @@ export class InputHandler {
     constructor(){
         this.keys = [];
         window.addEventListener('keydown', e => {
-            if ((e.key === "ArrowDown" || e.key === "ArrowDown") && this.keys.indexOf(e.key) === -1){
+            if ((e.key === "ArrowDown" || e.key === "ArrowDown" 
+                || e.key === "ArrowLeft" || e.key === "ArrowRight") && this.keys.indexOf(e.key) === -1){
                 this.keys.push(e.key);
             } 
         });
