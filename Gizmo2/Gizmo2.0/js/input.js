f7fb94a5f9d16bf6717301a0a9405122f9f059e5
Serge Cage <sergeycage@rocketmail.com>
2025-12-20 22:15:24 +0300

    feat: add event for arrowUp

--- Gizmo2/Gizmo2.0/js/input.js
+++ Gizmo2/Gizmo2.0/js/input.js
@@ -2,7 +2,7 @@ export class InputHandler {
     constructor(){
         this.keys = [];
         window.addEventListener('keydown', e => {
-            if (e.key === "ArrowDown" && this.keys.indexOf(e.key) === -1){
+            if ((e.key === "ArrowDown" || e.key === "ArrowDown") && this.keys.indexOf(e.key) === -1){
                 this.keys.push(e.key);
             } 
         });
