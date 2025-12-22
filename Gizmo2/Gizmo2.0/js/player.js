ba37b99911035d285a73a84a70ce44cf34082b33
Serge Cage <sergeycage@rocketmail.com>
2025-12-22 23:44:47 +0300

    feat: update horizontal movement

--- Gizmo2/Gizmo2.0/js/player.js
+++ Gizmo2/Gizmo2.0/js/player.js
@@ -12,13 +12,14 @@ export class Player {
         this.maxSpeed = 10;
     }
     update(input){
+        this.x += this.speed;
         if (input.includes("ArrowRight")) this.speed = this.maxSpeed;
         else if (input.includes("ArrowLeft")) this.speed = - this.maxSpeed;
         else this.speed = 0;
         if (this.x < 0 ) this.x = 0;
         if (this.x > this.game.width - this.width) this.x = this.game.width - this.width ;
 
-        if (input.includes("ArrowUp") && this.onGround()) this.vy -= 10;
+        if (input.includes("ArrowUp") && this.onGround()) this.vy -= 20;
         this.y += this.vy;
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
