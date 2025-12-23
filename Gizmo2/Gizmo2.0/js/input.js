711bb61a9c6b31065b9eca96f6e1357329c3f9fd
Serge Cage <sergeycage@rocketmail.com>
2025-12-23 23:31:07 +0300

    fix: fix input

--- Gizmo2/Gizmo2.0/js/input.js
+++ Gizmo2/Gizmo2.0/js/input.js
@@ -2,7 +2,7 @@ export class InputHandler {
     constructor(){
         this.keys = [];
         window.addEventListener('keydown', e => {
-            if ((e.key === "ArrowDown" || e.key === "ArrowDown" 
+            if ((e.key === "ArrowDown" || e.key === "ArrowUp" 
                 || e.key === "ArrowLeft" || e.key === "ArrowRight") && this.keys.indexOf(e.key) === -1){
                 this.keys.push(e.key);
             } 
