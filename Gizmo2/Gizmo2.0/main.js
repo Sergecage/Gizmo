29513ea126c3a1101a6fec99dde536f8db78415d
Serge Cage <sergeycage@rocketmail.com>
2025-12-24 23:30:14 +0300

    eat: set up timestamp to fix frames

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -25,7 +25,7 @@ window.addEventListener('load', function(){
     const game = new Game(canvas.width, canvas.height);
     let lastTime = 0;
 
-    const animate = () => {
+    const animate = (timeStamp) => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         game.update();
         game.draw(ctx);
