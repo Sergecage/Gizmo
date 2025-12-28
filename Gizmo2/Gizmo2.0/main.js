3f36385337a12ab7e6143e5e830e351dd0813010
Serge Cage <sergeycage@rocketmail.com>
2025-12-28 21:29:49 +0300

    feat: export background to main js

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -1,5 +1,6 @@
 import { Player } from "./js/player.js";
 import { InputHandler } from "./js/input.js";
+import { Background } from "./js/background.js";
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById("game-canvas-1");
