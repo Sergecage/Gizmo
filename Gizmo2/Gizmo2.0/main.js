cd735fc966f4bfb5372973cb6260db09f5deb871
Serge Cage <sergeycage@rocketmail.com>
2025-12-30 23:42:13 +0300

    feat: update speed of game

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -13,7 +13,7 @@ window.addEventListener('load', function(){
             this.width = width;
             this.height = height;
             this.groundMargin = 0;
-            this.speed = 3;
+            this.speed = 1;
             this.background = new Background(this);
             this.player = new Player(this);
             this.input = new InputHandler();
