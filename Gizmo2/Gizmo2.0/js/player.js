c88af93860d7c73b2321e060ddc144f8043d9e16
Serge Cage <sergeycage@rocketmail.com>
2026-02-07 22:33:11 +0300

    feat: add scoeon collision

--- Gizmo2/Gizmo2.0/js/player.js
+++ Gizmo2/Gizmo2.0/js/player.js
@@ -63,6 +63,7 @@ export class Player {
         this.game.enemies.forEach(enemy => {
             if (enemy.x < this.x + this.width && enemy.x + enemy.width > this.x &&  enemy.y < this.y + this.height && enemy.y + enemy.height > this.y){
                 enemy.markedForDeletion = true;
+                this.game.score++;
             } else {
 
             }
