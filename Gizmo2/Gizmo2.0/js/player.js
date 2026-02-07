9fb3aeb803cc718a95b65585c8e2982a48482d0c
Serge Cage <sergeycage@rocketmail.com>
2026-02-07 22:30:52 +0300

    feat: add conditin to check collision

--- Gizmo2/Gizmo2.0/js/player.js
+++ Gizmo2/Gizmo2.0/js/player.js
@@ -60,6 +60,12 @@ export class Player {
         this.currentState.enter();
     }
     checkCollisions(){
-        
+        this.game.enemies.forEach(enemy => {
+            if (enemy.x < this.x + this.width && enemy.x + enemy.width > this.x &&  enemy.y < this.y + this.height && enemy.y + enemy.height > this.y){
+                enemy.markedForDeletion = true;
+            } else {
+
+            }
+        });
     }
 };
