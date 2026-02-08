40675c989a6f4dd8eb03302b9ea47f3ff11af804
Serge Cage <sergeycage@rocketmail.com>
2026-02-08 21:23:31 +0300

    feat: add score to fame

--- Gizmo2/Gizmo2.0/js/player.js
+++ Gizmo2/Gizmo2.0/js/player.js
@@ -23,6 +23,7 @@ export class Player {
         this.currentState.enter();
     }
     update(input, deltaTime){
+        checkCollisions();
         this.currentState.handleInput(input);
         
         this.x += this.speed;
