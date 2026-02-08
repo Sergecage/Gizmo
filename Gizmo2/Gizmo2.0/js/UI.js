c35bb06e124097700f9d792b5e158f2bf5bdc165
Serge Cage <sergeycage@rocketmail.com>
2026-02-08 21:31:49 +0300

    feat: set up font for UI

--- Gizmo2/Gizmo2.0/js/UI.js
+++ Gizmo2/Gizmo2.0/js/UI.js
@@ -2,5 +2,9 @@ export class UI {
     constructor(game){
         this.game = game;
         this.fontSize = 30;
+        this.fontFamily = "Helvetica";
+    }
+    draw(context){
+        context.font = this.fontSize + "px" + this.fontFamily;
     }
 }
