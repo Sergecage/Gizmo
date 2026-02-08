7a87463cd7513bc23a819afe3068dee7dcd5c8c1
Serge Cage <sergeycage@rocketmail.com>
2026-02-08 21:37:42 +0300

    feat: import UI to main

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -2,6 +2,7 @@ import { Player } from "./js/player.js";
 import { InputHandler } from "./js/input.js";
 import { Background } from "./js/background.js";
 import { FlyingEnemy, GroundEnemy, CrawlingEnemy } from "./js/enemies.js";
+import { UI } from "./js/UI.js";
 
 window.addEventListener('load', function(){
     const canvas = document.getElementById("game-canvas-1");
