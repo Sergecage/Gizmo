be2121f1bf4517ed99e09c9ce01a9e4ec7c734a8
Serge Cage <sergeycage@rocketmail.com>
2026-02-09 16:32:47 +0300

    feat: add update method for particles

--- Gizmo2/Gizmo2.0/js/particles.js
+++ Gizmo2/Gizmo2.0/js/particles.js
@@ -1,5 +1,10 @@
 class Particle {
     constructor(game){
         this.game = game;
+        this.markedForDeletion = false;
+    }
+    update(){
+        this.x -= this.speedX + this.game.speed;
+        this.y -= this.speedY;
     }
 }
