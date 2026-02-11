4165f4399172335d708a19ef92a77fe560cb486a
Serge Cage <sergeycage@rocketmail.com>
2026-02-11 21:21:53 +0300

    feat:change dust color

--- Gizmo2/Gizmo2.0/js/particles.js
+++ Gizmo2/Gizmo2.0/js/particles.js
@@ -19,7 +19,7 @@ export class Dust extends Particle {
         this.y = y;
         this.speedX = Math.random();
         this.speedY = Math.random();
-        this.color = "Purple";
+        this.color = "#CCCCFF";
     };
     draw(context){
         context.beginPath();
