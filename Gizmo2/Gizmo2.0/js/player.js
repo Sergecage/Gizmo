d84c257dfb15e3817bc4d483769d87c17038eafe
Serge Cage <sergeycage@rocketmail.com>
2026-02-14 17:12:43 +0300

    feat: limit player vertical movement

--- Gizmo2/Gizmo2.0/js/player.js
+++ Gizmo2/Gizmo2.0/js/player.js
@@ -35,6 +35,8 @@ export class Player {
         if (!this.onGround()) this.vy += this.weight;
         else this.vy = 0;
 
+        if (this.y > this.game.height - this.height - this.game.groundMargin) this.y = this.game.height - this.height - this.game.groundMargin;
+
         if (this.currentState.animate) {
             if (this.frameTimer > this.frameInterval ) {
             this.frameTimer = 0;
