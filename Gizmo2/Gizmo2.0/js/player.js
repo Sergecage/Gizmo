ff4b1deda2df7256d1df34d99cb67173b30a649f
Serge Cage <sergeycage@rocketmail.com>
2026-02-15 22:15:26 +0300

    feat: update score on running state

--- Gizmo2/Gizmo2.0/js/player.js
+++ Gizmo2/Gizmo2.0/js/player.js
@@ -66,6 +66,7 @@ export class Player {
         this.game.enemies.forEach(enemy => {
             if (enemy.x < this.x + this.width && enemy.x + enemy.width > this.x &&  enemy.y < this.y + this.height && enemy.y + enemy.height > this.y){
                 enemy.markedForDeletion = true;
+                if (this.currentState === this.states[2])
             } else {
 
             }
