f0d400200c0487020f08fc73ce3e9239aed40439
Serge Cage <sergeycage@rocketmail.com>
2026-02-16 23:13:15 +0300

    fix: fix time display

--- Gizmo2/Gizmo2.0/js/UI.js
+++ Gizmo2/Gizmo2.0/js/UI.js
@@ -10,7 +10,7 @@ export class UI {
         context.fillStyle = this.game.fontColor;
         context.fillText("Score: " + this.game.score, 30 , 50);
         context.font  = this.fontSize * 0.8 + " px " +this.fontFamily;
-        context.fillText("Time: " + (this.game.time * 0.001), 20, 80);
+        context.fillText("Time: " + (this.game.time * 0.001).toFixed(1), 20, 80);
         if (this.game.gameOver){
             context.textAlign = "center";
             context.font  = this.fontSize * 2 + "px " +this.fontFamily;
