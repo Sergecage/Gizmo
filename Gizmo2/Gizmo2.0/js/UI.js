35d8772a7c55055cce1da0e3ff8b4e9c11c877b9
Serge Cage <sergeycage@rocketmail.com>
2026-02-16 23:07:11 +0300

    fix: fix text display

--- Gizmo2/Gizmo2.0/js/UI.js
+++ Gizmo2/Gizmo2.0/js/UI.js
@@ -9,11 +9,11 @@ export class UI {
         context.textAlign = "left";
         context.fillStyle = this.game.fontColor;
         context.fillText("Score: " + this.game.score, 30 , 50);
-        context.font  = this.fontSize * 0.8 + " px" +this.fontFamily;
+        context.font  = this.fontSize * 0.8 + " px " +this.fontFamily;
         context.fillText("Time: " + this.game.time, 20, 80);
         if (this.game.gameOver){
             context.textAlign = "center";
-            context.font  = this.fontSize * 2 + " px" +this.fontFamily;
+            context.font  = this.fontSize * 2 + "px " +this.fontFamily;
         }
     }
 }
