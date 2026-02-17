974ede90830c718579feb46dfeb95a1429dbadd8
Serge Cage <sergeycage@rocketmail.com>
2026-02-17 23:13:42 +0300

    feat: add lives to main js

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -31,6 +31,7 @@ window.addEventListener('load', function(){
             this.time = 0;
             this.maxTime = 10000;
             this.gameOver = false;
+            this.lives = 3;
             this.player.currentState =  this.player.states[0];
             this.player.currentState.enter();
         }
