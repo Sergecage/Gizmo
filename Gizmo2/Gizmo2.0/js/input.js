e76f6f99e38a28eb2d1b6fd14aeeb2d512507d02
Serge Cage <sergeycage@rocketmail.com>
2026-05-20 00:21:41 +0500

    fix: fix debug mode

--- Gizmo2/Gizmo2.0/js/input.js
+++ Gizmo2/Gizmo2.0/js/input.js
@@ -6,7 +6,7 @@ export class InputHandler {
             if ((e.key === "ArrowDown" || e.key === "ArrowUp" 
                 || e.key === "ArrowLeft" || e.key === "ArrowRight") && this.keys.indexOf(e.key) === -1){
                 this.keys.push(e.key);
-            } else if (e.key === "d") this.game.debug != this.game.debug;
+            } else if (e.key === "d") this.game.debug = !this.game.debug;
         });
         window.addEventListener("keyup", e => {
             if (e.key === "ArrowDown" || e.key === "ArrowUp" || e.key === "ArrowLeft" || e.key === "ArrowRight") {
