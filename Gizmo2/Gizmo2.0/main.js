d54f10d20ec95c28220f33661e465aab0725ef27
Serge Cage <sergeycage@rocketmail.com>
2026-05-27 22:15:56 +0300

    feat: draw cookies on screen

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -88,6 +88,9 @@ window.addEventListener('load', function(){
             });
             this.UI.draw(context);
             this.music.draw(context);
+            this.cookies.forEach(cookie => {
+                cookie.draw(context);
+            });
         }
         AddEnemy(){
             if (this.speed > 0 && Math.random() < 0.5 ) this.enemies.push(new GroundEnemy(this));
