7e9a9799b4ac3875780d1da3410eb9de8c25a9af
Serge Cage <sergeycage@rocketmail.com>
2026-05-27 21:38:35 +0300

    feat: set up the position of the cookie

--- Gizmo2/Gizmo2.0/js/cookie.js
+++ Gizmo2/Gizmo2.0/js/cookie.js
@@ -3,5 +3,9 @@ export class Cookie {
         this.game = game;
         this.spriteWidth = 150;
         this.spriteHeight = 190;
+        this.width = 150;
+        this.height = 190;
+        this.x = Math.random() * (this.width * 0.8);
+        this.y = Math.random() * (this.width * 0.8);
     }
 }
