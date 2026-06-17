4dc3fb07a5a7e67555ebd20729a818a38d1c126c
Serge Cage <sergeycage@rocketmail.com>
2026-06-17 21:42:18 +0300

    feat: add new backgrounds to class

--- Gizmo2/Gizmo2.0/js/background.js
+++ Gizmo2/Gizmo2.0/js/background.js
@@ -31,6 +31,8 @@ export class Background {
     this.height = 750;
     this.layerImage = layer1;
     this.layerImage2 = layer2;
+    this.layerImage3 = layer3;
+    this.layerImage4 = layer4;
     this.layer = new Layer(
       this.game,
       this.width,
@@ -44,6 +46,20 @@ export class Background {
       this.height,
       1,
       this.layerImage2,
+    );
+    this.layer3 = new Layer(
+      this.game,
+      this.width,
+      this.height,
+      1,
+      this.layerImage3,
+    );
+    this.layer4 = new Layer(
+      this.game,
+      this.width,
+      this.height,
+      1,
+      this.layerImage4,
     );
     this.layer.x = 0;
     this.layer2.x = this.width;
