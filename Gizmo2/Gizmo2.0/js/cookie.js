8a3f3d7f380657be103047bf102dd8253beceda2
Serge Cage <sergeycage@rocketmail.com>
2026-06-17 22:18:22 +0300

    feat: draw cookie by frame

--- Gizmo2/Gizmo2.0/js/cookie.js
+++ Gizmo2/Gizmo2.0/js/cookie.js
@@ -1,19 +1,28 @@
 export class Cookie {
-    constructor(game){
-        this.game = game;
-        this.spriteWidth = 150;
-        this.spriteHeight = 190;
-        this.width = 80;
-        this.height = 80;
-        this.x = Math.random() * (this.game.width - this.width);
-        this.y = Math.random() * (this.game.height - this.height - this.game.groundMargin);
-        this.image = document.getElementById("cookie");
-        this.markedForDeletion = false;
-    }
-    update(){
-
-    }
-    draw(context){
-        context.drawImage(this.image, this.x, this.y, this.width, this.height);
-    }
+  constructor(game) {
+    this.game = game;
+    this.spriteWidth = 150;
+    this.spriteHeight = 190;
+    this.width = 80;
+    this.height = 80;
+    this.x = Math.random() * (this.game.width - this.width);
+    this.y =
+      Math.random() * (this.game.height - this.height - this.game.groundMargin);
+    this.image = document.getElementById("cookie");
+    this.markedForDeletion = false;
+  }
+  update() {}
+  draw(context) {
+    context.drawImage(
+      this.image,
+      0,
+      0,
+      this.spriteWidth,
+      this.spriyeHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height,
+    );
+  }
 }
