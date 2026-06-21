ed30423beccb42024e5e6d2739128f0acda52cc9
Serge Cage <sergeycage@rocketmail.com>
2026-06-21 23:26:57 +0300

    feat: create h2 element

--- Gizmo2/Gizmo2.0/js/popUp.js
+++ Gizmo2/Gizmo2.0/js/popUp.js
@@ -3,3 +3,5 @@ const canvas = document.getElementById("game-canvas-1");
 const popup = document.createElement("div");
 popup.classList = "popup";
 canvas.append(popup);
+
+const welcomeText = document.createElement("h2");
