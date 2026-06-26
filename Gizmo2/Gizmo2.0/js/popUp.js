39f26404947dc35c6f1746a10b7ad776c77325d3
Serge Cage <sergeycage@rocketmail.com>
2026-06-26 23:26:46 +0300

    fix: wrap popup into function

--- Gizmo2/Gizmo2.0/js/popUp.js
+++ Gizmo2/Gizmo2.0/js/popUp.js
@@ -1,30 +1,32 @@
-const gameContainer = document.getElementById("game-container");
+export function showPop(startGame) {
+  const gameContainer = document.getElementById("game-container");
 
-const popup = document.createElement("div");
-popup.classList = "popup";
-gameContainer.append(popup);
+  const popup = document.createElement("div");
+  popup.classList = "popup";
+  gameContainer.append(popup);
 
-const welcomeText = document.createElement("h2");
-welcomeText.innerText = "Welcome to Gizmo Cookies 2";
-welcomeText.classList = "welcome-text";
+  const welcomeText = document.createElement("h2");
+  welcomeText.innerText = "Welcome to Gizmo Cookies 2";
+  welcomeText.classList = "welcome-text";
 
-const text = document.createElement("p");
-text.innerText = "Choose your level or play original hardcore one";
-text.classList = "text";
+  const text = document.createElement("p");
+  text.innerText = "Choose your level or play original hardcore one";
+  text.classList = "text";
 
-const buttonContainer = document.createElement("div");
-buttonContainer.classList = "btn-container";
-popup.append(welcomeText, text, buttonContainer);
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList = "btn-container";
+  popup.append(welcomeText, text, buttonContainer);
 
-const levelOne = document.createElement("button");
-levelOne.innerText = "Level 1";
-levelOne.classList = "btn-1";
+  const levelOne = document.createElement("button");
+  levelOne.innerText = "Level 1";
+  levelOne.classList = "btn-1";
 
-const levelTwo = document.createElement("button");
-levelTwo.innerText = "Level 2";
-levelTwo.classList = "btn-2";
+  const levelTwo = document.createElement("button");
+  levelTwo.innerText = "Level 2";
+  levelTwo.classList = "btn-2";
 
-const levelThree = document.createElement("button");
-levelThree.innerText = "Level 3";
-levelThree.classList = "btn-3";
-buttonContainer.append(levelOne, levelTwo, levelThree);
+  const levelThree = document.createElement("button");
+  levelThree.innerText = "Level 3";
+  levelThree.classList = "btn-3";
+  buttonContainer.append(levelOne, levelTwo, levelThree);
+}
