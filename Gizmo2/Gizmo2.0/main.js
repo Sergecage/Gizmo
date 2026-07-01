dc448dc5f9de14c80ebdd5a78aaa5769895bdbb9
Serge Cage <sergeycage@rocketmail.com>
2026-07-01 23:28:54 +0300

    feat: import showpop

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -5,6 +5,7 @@ import { FlyingEnemy, GroundEnemy, CrawlingEnemy } from "./js/enemies.js";
 import { UI } from "./js/UI.js";
 import { Music } from "./js/music.js";
 import { Cookie } from "./js/cookie.js";
+import { showPop } from "./js/popUp.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("game-canvas-1");
