78e8e0c36848dd7358decab329794b9a9f2ea8cd
Serge Cage <sergeycage@rocketmail.com>
2026-09-09 19:37:44 +0300

    feat: add update and draw methods to level one

--- Gizmo2/Gizmo2.0/js/levels/levelOne.js
+++ Gizmo2/Gizmo2.0/js/levels/levelOne.js
@@ -1,9 +1,11 @@
 class LevelOne {
-  constructor(game, width, height, image) {
+  constructor(game, width, height, speedModifier, image) {
     this.game = game;
     this.width = width;
     this.height = height;
     this.speedModifier = speedModifier;
     this.image = image;
   }
+  update() {}
+  draw() {}
 }
