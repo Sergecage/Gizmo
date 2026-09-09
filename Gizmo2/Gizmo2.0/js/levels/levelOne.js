2d589fdb73bc4962cb33c79000dd3cb54d204aed
Serge Cage <sergeycage@rocketmail.com>
2026-09-09 19:33:43 +0300

    feat: ad properties ti level one

--- Gizmo2/Gizmo2.0/js/levels/levelOne.js
+++ Gizmo2/Gizmo2.0/js/levels/levelOne.js
@@ -1,3 +1,9 @@
 class LevelOne {
-  constructor() {}
+  constructor(game, width, height, image) {
+    this.game = game;
+    this.width = width;
+    this.height = height;
+    this.speedModifier = speedModifier;
+    this.image = image;
+  }
 }
