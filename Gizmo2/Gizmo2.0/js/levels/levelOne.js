db1b06596778eacf395049ec8f7acbb1979b3ebf
Serge Cage <sergeycage@rocketmail.com>
2026-09-09 19:56:15 +0300

    feat: add backgrounds to level one

--- Gizmo2/Gizmo2.0/js/levels/levelOne.js
+++ Gizmo2/Gizmo2.0/js/levels/levelOne.js
@@ -1,5 +1,11 @@
 import { Background } from "../background";
 
 export const LevelOne = {
-  background: {},
+  background: {
+    layer1: "../../game/assets/img/Level1a.png",
+    layer2: "../../game/assets/img/Level1b.png",
+    layer3: "../../game/assets/img/Level1c.png",
+    layer4: "../../game/assets/img/Level1d.png",
+    layer5: "../../game/assets/img/Level1e.png",
+  },
 };
