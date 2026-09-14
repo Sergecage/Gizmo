b61b2f89378dea0b7ceaa02bbec3e96da951bff3
Serge Cage <sergeycage@rocketmail.com>
2026-09-15 00:13:07 +0300

    fix: fix export

--- Gizmo2/Gizmo2.0/main.js
+++ Gizmo2/Gizmo2.0/main.js
@@ -6,7 +6,7 @@ import { UI } from "./js/UI.js";
 import { Music } from "./js/music.js";
 import { Cookie } from "./js/cookie.js";
 import { showPop } from "./js/popUp.js";
-import { LevelOne, levelOne } from "./js/levels/levelOne.js";
+import { LevelOne } from "./js/levels/levelOne.js";
 
 const levels = {
   1: LevelOne,
