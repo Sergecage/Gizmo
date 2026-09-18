1b6fa052ae4b2750503aa52c446647d910b02629
Serge Cage <sergeycage@rocketmail.com>
2026-09-18 23:48:51 +0300

    fix: fix function name

--- Gizmo2/Gizmo2.0/js/background.js
+++ Gizmo2/Gizmo2.0/js/background.js
@@ -74,7 +74,7 @@ export class Background {
     this.layer4.x = this.width + this.width + this.width;
     this.backgroundLayers = [this.layer, this.layer2, this.layer3, this.layer4];
   }
-  reateImage(src) {
+  createImage(src) {
     const image = new Image();
     image.src = src;
     return image;
