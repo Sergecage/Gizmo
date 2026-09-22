ca3c5a0f4790f2826ef6f15fda079bd099e42fb2
Serge Cage <sergeycage@rocketmail.com>
2026-09-23 00:13:50 +0300

    feat: add buttons to controls

--- Gizmo2/Gizmo2.0/js/mobileControls.js
+++ Gizmo2/Gizmo2.0/js/mobileControls.js
@@ -1,5 +1,8 @@
 export class mobileControls {
   constructor(input) {
     this.input = input;
+    this.leftButton = document.querySelector("#left-button");
+    this.rightButton = document.querySelector("#right-button");
+    this.jumpButton = document.querySelector("#jump-button");
   }
 }
